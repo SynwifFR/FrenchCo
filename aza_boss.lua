9868a5305c0c7810de6e104b1202886f340f268a
SynwifFR <84232036+SynwifFR@users.noreply.github.com>
2021-05-21 15:20:51 +0200

    Actions patron vigneron

---
+++ aza_boss.lua
@@ -0,0 +1,103 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+local PlayerData = {}
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+     PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)  
+	PlayerData.job = job  
+	Citizen.Wait(5000) 
+end)
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(10)
+    end
+    while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+    end
+    if ESX.IsPlayerLoaded() then
+
+		ESX.PlayerData = ESX.GetPlayerData()
+
+    end
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerData = xPlayer
+end)
+
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	ESX.PlayerData.job = job
+end)
+
+
+---------------- boss ------------------
+
+RMenu.Add('aza', 'boss', RageUI.CreateMenu("LSPD", "Actions Patron"))
+Citizen.CreateThread(function()
+    while true do
+
+        RageUI.IsVisible(RMenu:Get('aza', 'boss'), true, true, true, function()
+
+            RageUI.Button("Accéder aux actions du patron",nil, {RightLabel = ""}, true, function(Hovered, Active, Selected)
+                if Selected then
+                    aboss()
+                    RageUI.CloseAll()
+                end
+            end)
+
+
+        end, function()
+        end, 1)
+                        Citizen.Wait(0)
+                                end
+                            end)
+
+---------------------------------------------
+
+local position = {
+    {x = 471.38, y = -1005.74, z = 30.69}
+}
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+        for k in pairs(position) do
+            if ESX.PlayerData.job and ESX.PlayerData.job.name == 'police' and ESX.PlayerData.job.grade_name == 'boss' then 
+
+            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, position[k].x, position[k].y, position[k].z)
+            DrawMarker(2, 471.38, -1005.74, 30.69, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0, 255, 0, 255, 0, 1, 2, 0, nil, nil, 0)
+
+        
+            if dist <= 1.0 then
+                ESX.ShowHelpNotification("Appuyez sur ~INPUT_TALK~ pour accéder au Actions Patron")
+                if IsControlJustPressed(1,51) then
+                    RageUI.Visible(RMenu:Get('aza', 'boss'), not RageUI.Visible(RMenu:Get('aza', 'boss')))
+                end
+            end
+        end
+    end
+    end
+end)
+
+function aboss()
+    TriggerEvent('esx_society:openBossMenu', 'vigne', function(data, menu)
+        menu.close()
+    end, {wash = false})
+end
+
+
+
