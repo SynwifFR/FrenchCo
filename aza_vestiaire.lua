a37bdaa581fcbb5443c9eaf3232f1b45d2c27bf9
SynwifFR <84232036+SynwifFR@users.noreply.github.com>
2021-05-21 15:09:53 +0200

    Vestiaire Vignerons

---
+++ aza_vestiaire.lua
@@ -0,0 +1,177 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+local PlayerData = {}
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+     PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)  
+	PlayerData.job = job  
+	Citizen.Wait(5000) 
+end)
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(10)
+    end
+    while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+    end
+    if ESX.IsPlayerLoaded() then
+
+		ESX.PlayerData = ESX.GetPlayerData()
+
+    end
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerData = xPlayer
+end)
+
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	ESX.PlayerData.job = job
+end)
+
+---------------- vestiaire ------------------
+
+RMenu.Add('aza', 'vigne', RageUI.CreateMenu("Vestiaire", "Vestiaire"))
+
+Citizen.CreateThread(function()
+    while true do
+
+        RageUI.IsVisible(RMenu:Get('aza', 'vigne'), true, true, true, function()
+
+            RageUI.Button("S'équiper de sa tenue : ~b~Civile",nil, {nil}, true, function(Hovered, Active, Selected)
+                if Selected then
+                    vcivil()
+                end
+            end)
+
+            RageUI.Button("S'équiper de la tenue : ~b~travaille",nil, {nil}, true, function(Hovered, Active, Selected)
+                if Selected then
+                    travail()
+                end
+            end)
+
+            
+        end, function()
+        end, 1)
+                        Citizen.Wait(0)
+                                end
+                            end)
+
+---------------------------------------------
+
+
+local position = {
+    {x = 473.85, y = -987.280, z = 25.73}
+}
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+        for k in pairs(position) do
+            if ESX.PlayerData.job and ESX.PlayerData.job.name == 'vigne' then 
+                DrawMarker(20, 388.90, -984.53, 27.98+1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.3, 0, 0, 255, 255, 0, 1, 2, 0, nil, nil, 0)
+
+
+
+            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, position[k].x, position[k].y, position[k].z)
+        
+            if dist <= 1.0 then
+                ESX.ShowHelpNotification("Appuyez sur ~INPUT_TALK~ pour accéder au vestiaire")
+                if IsControlJustPressed(1,51) then
+                    RageUI.Visible(RMenu:Get('aza', 'vigne'), not RageUI.Visible(RMenu:Get('aza', 'vigne')))
+                end
+            end
+        end
+    end
+    end
+end)
+
+function vpolice()
+                local model = GetEntityModel(GetPlayerPed(-1))
+                TriggerEvent('skinchanger:getSkin', function(skin)
+                    if model == GetHashKey("mp_m_freemode_01") then
+                        clothesSkin = {
+                            ['bags_1'] = 0, ['bags_2'] = 0,
+                            ['tshirt_1'] = 39, ['tshirt_2'] = 0,
+                            ['torso_1'] = 55, ['torso_2'] = 0,
+                            ['arms'] = 30,
+                            ['pants_1'] = 46, ['pants_2'] = 0,
+                            ['shoes_1'] =25, ['shoes_2'] = 0,
+                            ['mask_1'] = 0, ['mask_2'] = 0,
+                            ['bproof_1'] = 0,
+                            ['chain_1'] = 0,
+                            ['helmet_1'] = -1, ['helmet_2'] = 0,
+                        }
+                    else
+                        clothesSkin = {
+                            ['bags_1'] = 0, ['bags_2'] = 0,
+                            ['tshirt_1'] = 15,['tshirt_2'] = 2,
+                            ['torso_1'] = 65, ['torso_2'] = 2,
+                            ['arms'] = 36, ['arms_2'] = 0,
+                            ['pants_1'] = 38, ['pants_2'] = 2,
+                            ['shoes_1'] = 12, ['shoes_2'] = 6,
+                            ['mask_1'] = 0, ['mask_2'] = 0,
+                            ['bproof_1'] = 0,
+                            ['chain_1'] = 0,
+                            ['helmet_1'] = -1, ['helmet_2'] = 0,
+                        }
+                    end
+                    TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
+                end)
+            end
+
+            function travail()
+                local model = GetEntityModel(GetPlayerPed(-1))
+                TriggerEvent('skinchanger:getSkin', function(skin)
+                    if model == GetHashKey("mp_m_freemode_01") then
+                        clothesSkin = {
+                            ['bags_1'] = 0, ['bags_2'] = 0,
+                            ['tshirt_1'] = 39, ['tshirt_2'] = 0,
+                            ['torso_1'] = 52, ['torso_2'] = 0,
+                            ['arms'] = 31,
+                            ['pants_1'] = 46, ['pants_2'] = 0,
+                            ['shoes_1'] =25, ['shoes_2'] = 0,
+                            ['mask_1'] = 0, ['mask_2'] = 0,
+                            ['bproof_1'] = 0,
+                            ['chain_1'] = 0,
+                            ['helmet_1'] = -1, ['helmet_2'] = 0,
+                        }
+                    else
+                        clothesSkin = {
+                            ['bags_1'] = 0, ['bags_2'] = 0,
+                            ['tshirt_1'] = 15,['tshirt_2'] = 2,
+                            ['torso_1'] = 65, ['torso_2'] = 2,
+                            ['arms'] = 36, ['arms_2'] = 0,
+                            ['pants_1'] = 38, ['pants_2'] = 2,
+                            ['shoes_1'] = 12, ['shoes_2'] = 6,
+                            ['mask_1'] = 0, ['mask_2'] = 0,
+                            ['bproof_1'] = 0,
+                            ['chain_1'] = 0,
+                            ['helmet_1'] = -1, ['helmet_2'] = 0,
+                        }
+                    end
+                    TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
+                end)
+            end
+
+            
+
+function vcivil()
+    ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+        TriggerEvent('skinchanger:loadSkin', skin)
+       end)
+    end
